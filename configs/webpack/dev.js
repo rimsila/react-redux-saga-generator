2ac43c15e88a735f6025799b8930ba681d97d1e1
Sila Rim <rimsila.itc@gmail.com>
2020-10-11 04:31:25 +0700

    env enviroment

--- configs/webpack/dev.js
+++ configs/webpack/dev.js
@@ -1,23 +1,28 @@
 // development config
-const merge = require('webpack-merge');
-const webpack = require('webpack');
-const commonConfig = require('./common');
+const merge = require("webpack-merge");
+const webpack = require("webpack");
+const commonConfig = require("./common");
+const { join } = require("path");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = merge(commonConfig, {
-  mode: 'development',
+  mode: "development",
   entry: [
-    'react-hot-loader/patch', // activate HMR for React
-    'webpack-dev-server/client?http://localhost:8080',// bundle the client for webpack-dev-server and connect to the provided endpoint
-    'webpack/hot/only-dev-server', // bundle the client for hot reloading, only- means to only hot reload for successful updates
-    './index.js' // the entry point of our app
+    "react-hot-loader/patch", // activate HMR for React
+    "webpack-dev-server/client?http://localhost:8080", // bundle the client for webpack-dev-server and connect to the provided endpoint
+    "webpack/hot/only-dev-server", // bundle the client for hot reloading, only- means to only hot reload for successful updates
+    "./index.js", // the entry point of our app
   ],
 
   devServer: {
     open: true,
   },
-  
-  devtool: 'cheap-module-eval-source-map',
+
+  devtool: "cheap-module-eval-source-map",
   plugins: [
     new webpack.HotModuleReplacementPlugin(), // enable HMR globally
+    new Dotenv({
+      path: join(__dirname, "../../.env.dev") // load this now instead of the ones in '.env'
+    })
   ],
 });
--- src/page/NotFound/index.tsx
+++ src/page/NotFound/index.tsx
@@ -1,20 +1,17 @@
 import * as React from "react";
 import "./styles.css";
-import { Link } from "react-router-dom";
-import { ROUTE_PATH } from "@/utils/route-util";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "@/redux-store/users/actions";
-import { createSelector } from "reselect";
 import { usersState } from "@/redux-store/users/selectors";
 const NotFound = () => {
   const dispatch = useDispatch();
   const {
-
     booleanable: { isFetchUsersInProgress, isUpdate },
   } = useSelector(usersState());
 
+  console.log(process.env.REACT_APP_STAGE);
+
   if (isFetchUsersInProgress) return <h1>Loading.....</h1>;
-  console.log("isUpdate", isUpdate);
 
   return (
     <div id="notfound">
