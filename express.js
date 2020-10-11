67954665b173b03cf8c4c79fe75fcebf2e763961
Sila Rim <rimsila.itc@gmail.com>
2020-10-12 03:30:13 +0700

    make it better

--- express.js
+++ express.js
@@ -1,9 +1,19 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
 const portNumber = 5000;
-const sourceDir = 'build';
+const sourceDir = "build";
+const cors = require("cors");
 
-app.use(express.static(sourceDir));
+app.use(express.static(path.join(__dirname, sourceDir)));
+app.use(cors());
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, sourceDir, "index.html"));
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, sourceDir, "index.html"));
+});
 
 app.listen(portNumber, () => {
   console.log(`Express web server started: http://localhost:${portNumber}`);
